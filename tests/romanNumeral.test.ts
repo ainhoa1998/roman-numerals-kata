baca2df2b47934f37c1216a72375305a1787cce2
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-17 16:42:44 +0200

    Refactor: naming

--- tests/romanNumeral.test.ts
+++ tests/romanNumeral.test.ts
@@ -1,7 +1,7 @@
 import { romanNumeral } from '../src/romanNumeral'
 
 describe('Kata Roman Numerals', () => {
-  it('El elemento 1 devolverá el string I', () => {
+  it('El número 1 es I', () => {
     const result = romanNumeral()
 
     expect(result).toBe('I')
