ed597113c255b1183067298d8f70b0137b8028be
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-06-18 17:31:39 +0200

    Refactor: crea diccionario

--- src/romanNumeral.ts
+++ src/romanNumeral.ts
@@ -1,45 +1,48 @@
+interface Dictionary {
+  arabic: number
+  roman: string
+}
+
 export function romanNumeral(arabic: number): string {
-  const dictionary = [
+  const dictionary: Dictionary[] = [
     {
       arabic: 1,
       roman: 'I',
+    },
+    {
+      arabic: 4,
+      roman: 'IV',
     },
     {
       arabic: 5,
       roman: 'V',
     },
     {
+      arabic: 9,
+      roman: 'IX',
+    },
+    {
       arabic: 10,
       roman: 'X',
     },
+    { arabic: 14, roman: 'XIV' },
+    { arabic: 19, roman: 'XIX' },
   ]
   let result = ''
   let value = arabic
 
-  if (value === 19) {
-    return 'XIX'
+  if (dictionary.some((entry) => entry.arabic === arabic)) {
+    return dictionary.find((entry) => entry.arabic === arabic).roman
   }
 
-  if (value === 14) {
-    return 'XIV'
-  }
-
-  if (value === 9) {
-    return 'IX'
-  }
-
-  if (value === 4) {
-    return 'IV'
+  while (value > dictionary[4].arabic - 1) {
+    result += dictionary[4].roman
+    value -= dictionary[4].arabic
   }
 
   while (value > dictionary[2].arabic - 1) {
     result += dictionary[2].roman
     value -= dictionary[2].arabic
-  }
-
-  while (value > dictionary[1].arabic - 1) {
-    result += dictionary[1].roman
-    value -= dictionary[1].arabic
   }
 
   while (value > dictionary[0].arabic - 1) {
